a5dac20af3ce3e009e0361f6fdd3884ef3957bff
amitkolkar <54172454+amitkolkar@users.noreply.github.com>
2022-07-07 23:56:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-  return "Congratulations, its a web app!"
+  return "Congratulations, its a web app! Update 1"
 
 if __name__ == "__main__" :
   app.run(host="0.0.0.0", port=8080)
