3cd3ce184f9f349b4d54531a6fc044f02ea491a5
amitkolkar <54172454+amitkolkar@users.noreply.github.com>
2022-07-07 22:54:55 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+#main.py
+from flask import Flask
+app = Flask(_name_)
+
+@app.route("/")
+def index():
+  return "Congratulations, its a web app!"
+
+if __name__ == "__main__" :
+  app.return(host="127.0.0.1", port=8080, debug=True)
